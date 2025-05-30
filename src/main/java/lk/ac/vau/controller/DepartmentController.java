2774257fb6af776a86ab0a0f8a78efa9b91c30d7
Ravindu Hirusanka <puresoftlk@gmail.com>
2025-05-31 00:37:36 +0530

    Implement DepartmentController with CRUD operations and enhance GenericExceptionHandler for error responses

--- src/main/java/lk/ac/vau/controller/DepartmentController.java
+++ src/main/java/lk/ac/vau/controller/DepartmentController.java
@@ -1,15 +1,67 @@
 package lk.ac.vau.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lk.ac.vau.model.Department;
+import lk.ac.vau.repo.DepartmentRepo;
 import lk.ac.vau.service.DepartmentService;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @RestController
 @RequestMapping("/departments")
 public class DepartmentController {
     @Autowired
     private DepartmentService service;
+
+    @Autowired
+    private DepartmentRepo departmentRepo;
+
+
+    //Get all departments
+    @GetMapping()
+    public ResponseEntity<List<Department>> getAllDepartments(){
+        return new ResponseEntity<List<Department>>
+        (service.getAllDepartments(),HttpStatus.OK);
+    }
+
+    //Get department by ID
+    @GetMapping("/{id}")
+    public ResponseEntity<Department> getDepartmentById(@PathVariable("id") Long depId){
+        return new ResponseEntity<Department>(service.getDepartmentById(depId),HttpStatus.OK);
+    }
+
+    //Delete a department
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteDepartment(@PathVariable Long id){
+        if (departmentRepo.existsById(id)){
+            departmentRepo.deleteById(id);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+
+
+
+
+    
+
+
+    
+
+
+
     
 }
--- src/main/java/lk/ac/vau/exceptionHandler/GenericExceptionHandler.java
+++ src/main/java/lk/ac/vau/exceptionHandler/GenericExceptionHandler.java
@@ -1,5 +1,17 @@
 package lk.ac.vau.exceptionHandler;
 
-public class GenericExceptionHandler {
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import jakarta.persistence.EntityNotFoundException;
+import lk.ac.vau.model.ErrorResponse;
+
+@RestControllerAdvice
+public class GenericExceptionHandler {
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleEntityNotFound(EntityNotFoundException exception){
+        ErrorResponse errorResponse =new ErrorResponse(HttpStatus.NOT_FOUND.value(),exception.getMessage().toString());
+    }
 }
